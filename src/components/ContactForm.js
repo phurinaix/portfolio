8bf25ab46baf4733796de701993dee5434255ee6
phurinaix <phurinaix@gmail.com>
2019-06-20 16:43:54 +0700

    create send email function

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import { Form, Button } from 'react-bootstrap';
+import ReCAPTCHA from "react-google-recaptcha";
 
 const ContactForm = (props) => {
     const { topic, description, fName, fEmail, fSubject, fMessage, fSubmit } = props;
     return (
-        <Form className="col-md-10 col-lg-9 m-auto">
+        <Form className="col-md-10 col-lg-9 m-auto" onSubmit={props.submit}>
             <h3>{topic}</h3>
             <p>{description}</p>
             <Form.Group controlId="formBasicName">
-                <Form.Control type="text" placeholder={fName} />
+                <Form.Control type="text" placeholder={fName} name="name" value={props.name} onChange={props.change} required/>
             </Form.Group>
             <Form.Group controlId="formBasicEmail">
-                <Form.Control type="email" placeholder={fEmail} />
+                <Form.Control type="email" placeholder={fEmail} name="email" value={props.email} onChange={props.change} required/>
             </Form.Group>
             <Form.Group controlId="formBasicSubject">
-                <Form.Control type="text" placeholder={fSubject} />
+                <Form.Control type="text" placeholder={fSubject} name="subject" value={props.subject} onChange={props.change} required/>
             </Form.Group>
             <Form.Group controlId="exampleForm.ControlTextarea1">
-                <Form.Control as="textarea" rows="6" placeholder={fMessage} />
+                <Form.Control as="textarea" rows="6" placeholder={fMessage} name="message" value={props.message} onChange={props.change} required/>
             </Form.Group>
-            <Button type="button">
+            <ReCAPTCHA
+                sitekey="6LeS0qkUAAAAAIHVnVo2kX4OVgJudz24g8HyqhAV"
+                // onChange={onChange}
+            />
+            <Button type="submit" disabled={props.fetching}>
                 {fSubmit}
             </Button>
         </Form>
--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -8,18 +8,19 @@ import GithubCode from '../assets/img/qrcode/github.png';
 // import FacebookLogo from '../../assets/img/icon/facebook.svg';
 import LinkedinLogo from '../assets/img/icon/linkedin.svg';
 import GithubLogo from '../assets/img/icon/github.svg';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const locale = require('react-redux-i18n').I18n;
 
 class Contact extends Component {
     state = {
+        name: '',
+        email: '',
+        subject: '',
+        message: '',
+        fetching: false,
         socials: [
-            // { 
-            //     name: 'Facebook',
-            //     link: 'https://www.facebook.com/phurinat.puekkham.1',
-            //     qrcode: FacebookCode,
-            //     logo: FacebookLogo
-            // },
             { 
                 name: 'Github',
                 link: 'https://github.com/phurinaix',
@@ -34,6 +35,64 @@ class Contact extends Component {
             }
         ]
     }
+    notify = () => toast.success("Your e-mail has been successfully sent. Thank You!", {
+        position: toast.POSITION.BOTTOM_RIGHT
+    });
+    onChangeHandler = (e) => {
+        this.setState({[e.target.name]: e.target.value});
+    }
+    onSubmitHandler = (e) => {
+        e.preventDefault();
+        this.setState({ fetching: true });
+        const data = {
+            name: this.state.name,
+            email: this.state.email,
+            subject: this.state.subject,
+            message: this.state.message
+        };
+        console.log(JSON.stringify(data));
+        fetch('http://localhost:3001/email', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+                var status = result.status;
+                this.setState({ fetching: false });
+                if (status === "success") {
+                    toast.success("Your e-mail has been successfully sent. Thank You!", {
+                        position: toast.POSITION.BOTTOM_RIGHT
+                    });
+                    this.setState({
+                        name: '',
+                        email: '',
+                        subject: '',
+                        message: ''
+                    });
+                } else if (status === "empty_error") {
+                    toast.error("Form can't be empty", {
+                        position: toast.POSITION.BOTTOM_RIGHT
+                    });
+                } else if (status === "email_error") {
+                    toast.error("Email is invalid", {
+                        position: toast.POSITION.BOTTOM_RIGHT
+                    });
+                } else if (status === "error") {
+                    toast.error("Error", {
+                        position: toast.POSITION.BOTTOM_RIGHT
+                    });
+                }
+            })
+            .catch(e => {
+                toast.error("Error", {
+                    position: toast.POSITION.BOTTOM_RIGHT
+                });
+            });
+    }
     render() {
         return (
             <div className="contact-section text-center">
@@ -42,6 +101,13 @@ class Contact extends Component {
                         <ContactForm
                             topic={locale.t('contact.topic')}
                             description={locale.t('contact.description')}
+                            change={this.onChangeHandler}
+                            submit={this.onSubmitHandler}
+                            name={this.state.name}
+                            email={this.state.email}
+                            subject={this.state.subject}
+                            message={this.state.message}
+                            fetching={this.state.fetching}
                             fName={locale.t('contact.form.name')}
                             fEmail={locale.t('contact.form.email')}
                             fSubject={locale.t('contact.form.subject')}
@@ -49,6 +115,7 @@ class Contact extends Component {
                             fSubmit={locale.t('contact.form.submit')}
                         />
                     </div>
+                    <ToastContainer autoClose={5000} hideProgressBar/>
                     <div className="col-md-4">
                         {this.state.socials.map((social, index) => {
                             return <a href={social.link} target="_blank" rel="noopener noreferrer" key={index}><ContactBox qrcode={social.qrcode} title={social.name} logo={social.logo} /></a>
